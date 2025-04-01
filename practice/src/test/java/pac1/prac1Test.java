33d4cc291cd760d6541d0264fd78cf0c375a2d2c
mokshithnarekodi <mokshithnarekodi@gmail.com>
2025-04-01 14:53:04 +0530

    PUSH1

---
+++ practice/src/test/java/pac1/prac1Test.java
@@ -0,0 +1,24 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class prac1Test {
+	@Test
+	public void test1() {
+		String url=System.getProperty("url");
+		String browser=System.getProperty("browser");
+		String username=System.getProperty("username");
+		String password=System.getProperty("password");
+		
+		System.out.println(url);
+		System.out.println(browser);
+		System.out.println(username);
+		System.out.println(password);
+		System.out.println("test1");
+	}
+	@Test
+	public void test2() {
+		System.out.println("test2");
+	}
+
+}
---
+++ practice/src/test/java/pac2/prac2Test.java
@@ -0,0 +1,15 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class prac2Test {
+	@Test
+	public void test3() {
+		System.out.println("test3");
+	}
+	@Test
+	public void test4() {
+		System.out.println("test4");
+	}
+
+}
